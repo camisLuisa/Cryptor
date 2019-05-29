140be180d29d6c1c74d4c00f1ffe0c976e47eeda
cllima <cllima2@stefanini.com>
2019-05-29 08:51:24 -0300

    Create project

---
+++ Cryptor/Cryptor/Cryptor.h
@@ -0,0 +1,13 @@
+//
+//  Cryptor.h
+//  Cryptor
+//
+//  Created by Camila Luísa on 29/05/2019.
+//  Copyright © 2019 Luisa, Camila. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Cryptor : NSObject
+
+@end
---
+++ Cryptor/Cryptor/Cryptor.m
@@ -0,0 +1,13 @@
+//
+//  Cryptor.m
+//  Cryptor
+//
+//  Created by Camila Luísa on 29/05/2019.
+//  Copyright © 2019 Luisa, Camila. All rights reserved.
+//
+
+#import "Cryptor.h"
+
+@implementation Cryptor
+
+@end
